bbc3bc71950978589cb02777e5583cdb396ce525
Sebastian Schmittner <schmittner@eecc.info>
2020-09-30 23:50:18 +0200

    'deployment'

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,6 +3,7 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
+    await execa("git", ["commit", "-am", "'deployment'"]);
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
